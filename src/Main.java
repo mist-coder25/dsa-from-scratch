130bda146d54f7dc410dca0508b6b4a039fe6180
mist-coder25 <snehal.thube22@mmit.edu.in>
2026-03-23 23:59:54 +0530

    Day 1: Fibonacci

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        if(n == 0) return;
+        int prev2 = 0;
+        System.out.print(prev2 + " ");
+
+        if(n == 1) return;
+        int prev1 = 1;
+        System.out.print(prev1 + " ");
+
+        for(int i = 2; i < n; i++){
+            int newFibonacciNumber = prev1 + prev2;
+            System.out.print(newFibonacciNumber + " ");
+            prev2 = prev1;
+            prev1 = newFibonacciNumber;
+        }
+        sc.close();
+
+    }
+}
